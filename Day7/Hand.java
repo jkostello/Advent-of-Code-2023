c1330a88d512bb30f15e0eae574984f7bf2b4140
jkostello <joseph.kostello@gmail.com>
2024-04-10 17:24:05 -0500

    Removed unnecessary checkStrength() function

--- Day7/Hand.java
+++ Day7/Hand.java
@@ -9,22 +9,6 @@ public class Hand implements Comparable<Hand> {
     private HashMap<Character, Integer> cardMap = new HashMap<>();
     private char[] cardStrength = {'2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'};
 
-    private int checkStrength() {
-        if(cardMap.size() == 2) { // Check four of a kind or Full house
-            if (cardMap.containsValue(4)) {
-                return 6; // Four of a kind
-            } else {
-                return 5; // Full house
-            }
-        } else { // Check Three of a kind or Two pair
-            if (cardMap.containsValue(3)) {
-                return 4; // Three of a kind
-            } else {
-                return 3; // Two pair
-            }
-        }
-    }
-
     public Hand(String input) {
         cards = input.split(" ")[0].toCharArray();
         bet = Integer.parseInt(input.split(" ")[1]);
@@ -45,10 +29,18 @@ public class Hand implements Comparable<Hand> {
             this.strength = 7; // Five of a kind
             break;
         case 2:
-            this.strength = checkStrength(); // Four of a kind / Full house
+            if (cardMap.containsValue(4)) {
+                this.strength = 6; // Four of a kind
+            } else {
+                this.strength = 5; // Full house
+            }
             break;
         case 3:
-            this.strength = checkStrength(); // Three of a kind / Two pair
+            if (cardMap.containsValue(3)) {
+                this.strength = 4; // Three of a kind
+            } else {
+                this.strength = 3; // Two pair
+            }
             break;
         case 4:
             this.strength = 2; // One pair
